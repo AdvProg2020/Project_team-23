eb8b7298211296c6c05dff92a42410c50d06f665
sarazm2000 <sarazm.2000@gmail.com>
2020-05-04 23:34:29 +0430

    coded discount

--- src/com/company/model/shopping/CodedDiscount.java
+++ src/com/company/model/shopping/CodedDiscount.java
@@ -1,5 +1,41 @@
 package com.company.model.shopping;
 
-public class CodedDiscount {
+import com.company.model.user.Customer;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CodedDiscount {
+    private ArrayList<Customer> customersDiscount;//customers have discount code
+    private double amountOfDiscount;//persent
+    private Date startDate;
+    private Date finishDate;
+    private int repeatedDiscount;
+
+    public CodedDiscount(double amountOfDiscount, Date startDate, Date finishDate) {
+        this.amountOfDiscount = amountOfDiscount;
+        this.startDate = startDate;
+        this.finishDate = finishDate;
+        this.repeatedDiscount = repeatedDiscount;
+    }
+
+    public ArrayList<Customer> getCustomersDiscount() {
+        return customersDiscount;
+    }
+
+    public void setCustomersDiscount(ArrayList<Customer> customersDiscount) {
+        this.customersDiscount = customersDiscount;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    private int addRepeatedDiscount(){
+        return repeatedDiscount++;
+    }
 }
