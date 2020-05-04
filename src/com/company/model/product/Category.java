5b4ea37980fc1ef4f713b3a28e0d98e115f64e0f
sarazm2000 <sarazm.2000@gmail.com>
2020-05-04 23:43:18 +0430

    category class

--- src/com/company/model/product/Category.java
+++ src/com/company/model/product/Category.java
@@ -5,7 +5,47 @@ package com.company.model.product;
     public class Category {
         private String name;
         private String property;
+        private static ArrayList<Category> allCategories = new ArrayList<>();
         private ArrayList<Category> subCategory;
         private ArrayList<Product> productsList;
+
+        public Category(String name){
+            this.name = name;
+            allCategories.add(this);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Category findCategoryByName(String name){
+            for (Category allCategory : allCategories) {
+                if(allCategory.getName().equals(name)){
+                    return allCategory;
+                }
+            }
+            return null;
+        }
+
+        public void addProductToCategory(Product product){
+            productsList.add(product);
+        }
+
+        public void removeProductFromCategory(Product product){
+            productsList.remove(product);
+        }
+
+        public void addSubCategory(Category category){
+            subCategory.add(category);
+        }
+
+        public void removeSubCategory(Category category){
+            subCategory.remove(category);
+        }
+
+        public void removeCategory(Category category){
+            allCategories.remove(category);
+        }
+
     }
 
