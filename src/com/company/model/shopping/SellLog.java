e3cc4eefba724074d59a291f42ae82d129bd8107
sarazm2000 <sarazm.2000@gmail.com>
2020-05-08 23:41:30 +0430

    category class

--- src/com/company/model/shopping/SellLog.java
+++ src/com/company/model/shopping/SellLog.java
@@ -3,11 +3,21 @@ package com.company.model.shopping;
 
 import com.company.model.product.Product;
 import com.company.model.shopping.BuyAndSellLog;
+import com.company.model.status.ProcessesForBuy;
+import com.company.model.user.Seller;
 
 import java.util.ArrayList;
+import java.util.Date;
 
-    public class SellLog extends BuyAndSellLog {
+public class SellLog extends BuyAndSellLog {
         private ArrayList<Product> sellProducts;
         private double getMoney;
+        private double discountMoney;
+        private String earmark;
+        private Date date;
+        private ArrayList<Product> soldProducts;
+        private Seller seller;
+        private ProcessesForBuy processesForBuy;
+
     }
 
