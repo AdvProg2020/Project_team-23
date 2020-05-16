187f136fc68bd2474088cf08f036b54dbfb5001a
sarazm2000 <sarazm.2000@gmail.com>
2020-05-16 19:34:12 +0430

    register menu in view

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,4 @@ public class Main {
         CommandPro.recognizeCommand(command);
     }
 
-    public void getScanner(){
-
-    }
 }
--- src/com/company/view/RegisterPanel.java
+++ src/com/company/view/RegisterPanel.java
@@ -1,8 +1,8 @@
 package com.company.view;
 
-
 import com.company.controller.RegisterAndLoginController;
 import com.company.model.user.Roles;
+import java.util.Scanner;
 
 public class RegisterPanel extends Menu {
     private String username;
@@ -28,68 +28,79 @@ public class RegisterPanel extends Menu {
     }
 
     public void registerAccount() {
-        System.out.println("enter username: ");
+
         chooseUsername();
-
-        System.out.println("enter first name and last name: ");
-        enterFirstNameAndLastName();
-
-        System.out.println("enter your Email: ");
-        enterEmail();
-
-        System.out.println("enter your phone: ");
-        enterPhone();
-
-        System.out.println("password: ");
-        enterPassword();
-
-        System.out.println("choose your role: seller or customer? ");
 
     }
 
     public void chooseUsername() {
+        System.out.println("enter username: ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
         RegisterAndLoginController registerAndLoginController = new RegisterAndLoginController();
-        username = "SaraZM200";
-        registerAndLoginController.setUsername(username);
+        registerAndLoginController.setUsername(input);
+        System.out.println(input);
+
+        enterFirstNameAndLastName();
     }
 
     public void enterFirstNameAndLastName() {
+        System.out.println("enter first name and last name: ");
         RegisterAndLoginController registerAndLoginController = new RegisterAndLoginController();
-        firstName = "Sara";
-        lastName = "Zahedi";
-        registerAndLoginController.setFirstName(firstName);
-        registerAndLoginController.setLastName(lastName);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String name[] = input.split("\\s+");
+        registerAndLoginController.setFirstName(name[0]);
+        registerAndLoginController.setLastName(name[1]);
+        System.out.println("firstName: " + name[0] + ", lastName: " + name[1]);
+
+        enterEmail();
     }
 
     public void enterEmail() {
+        System.out.println("enter your Email: ");
         RegisterAndLoginController registerAndLoginController = new RegisterAndLoginController();
-        Email = "sarazm@gmail.com";
-        registerAndLoginController.setEmail(Email);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        registerAndLoginController.setEmail(input);
+        System.out.println("Email: " + input);
+
+        enterPhone();
     }
 
     public void enterPhone() {
+        System.out.println("enter your phone: ");
         RegisterAndLoginController registerAndLoginController = new RegisterAndLoginController();
-        phone = "09121234567";
-        registerAndLoginController.setPhone(phone);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        registerAndLoginController.setPhone(input);
+        System.out.println("phone: " + input);
+
+        enterPassword();
     }
 
     public void enterPassword() {
+        System.out.println("password: ");
         RegisterAndLoginController registerAndLoginController = new RegisterAndLoginController();
-        password = "1234567";
-        registerAndLoginController.setPassword(password);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        registerAndLoginController.setPassword(input);
+        System.out.println("password: " + input);
+
+        chooseRole();
     }
 
     public void chooseRole() {
         RegisterAndLoginController registerAndLoginController = new RegisterAndLoginController();
+        Scanner sc = new Scanner(System.in);
         while (true) {
-            if (roles == Roles.CUSTOMER) {
+            System.out.println("choose your role: seller or customer? ");
+            String input = sc.nextLine();
+            if ((input.equalsIgnoreCase("customer")) || ((input.equalsIgnoreCase("seller")))) {
                 registerAndLoginController.setRoles(roles);
                 break;
-            } else if (roles == Roles.SELLER) {
-                registerAndLoginController.setRoles(roles);
-                break;
-            } else {
-                System.out.println("You must choose customer role or seller role! ");
+            }else {
+                System.out.println("You must choose seller or customer role! ");
                 continue;
             }
         }
